fae49d59df6660e3ef9457bc46f98e26ffeb2183
A00303235 <A00303235@mycambrian.ca>
2024-09-28 14:24:46 -0400

    Created all methods

--- Calc_API/Controllers/CalcController.cs
+++ Calc_API/Controllers/CalcController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Calc_API.Controllers
@@ -7,5 +8,45 @@ namespace Calc_API.Controllers
     [ApiController]
     public class CalcController : ControllerBase
     {
+        [HttpGet("Add")]
+        public IActionResult Add(decimal num1, decimal num2)
+        {
+            var result = num1 + num2;
+            return Ok(result);
+        }
+
+        [HttpGet("Subtract")]
+        public IActionResult Subtract(decimal num1, decimal num2)
+        {
+            var result = num1 - num2;
+            return Ok(result);
+        }
+
+        [HttpGet("Multiply")]
+        public IActionResult Multiply(decimal num1, decimal num2)
+        {
+            var result = num1 * num2;
+            return Ok(result);
+        }
+
+        [HttpGet("Division")]
+        public IActionResult Divide(decimal num1, decimal num2)
+        {
+            
+            var result = num1 / num2;
+            return Ok(result);
+        }
+
+        [HttpGet ("Modulo")]
+        public IActionResult Modulo(decimal num1, decimal num2)
+        {
+            if (num2 == 0)
+            {
+                return BadRequest("Cannot divide by zero.");
+            }
+            var result = num1 % num2;
+            return Ok(result);
+        }
+
     }
 }
